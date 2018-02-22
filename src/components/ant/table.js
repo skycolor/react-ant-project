31b24fbeffabffc2ecfb9c260216715e9d8dfde7
skycolor <873276172@qq.com>
2018-02-22 16:14:57 +0800

    表格

--- src/components/ant/table.js
+++ src/components/ant/table.js
@@ -1,19 +1,62 @@
 import React from 'react';
 import {connect} from 'react-redux'
+import { Table, Button , Popconfirm } from 'antd';
 
 import Bread from '../include/bread'
 
-class Table extends React.Component {
+//获取table的数据
+import userData from '../../data/userData.json';
+
+class TablePage extends React.Component {
   constructor(props) {
     super(props);
+    this.columns = [{
+      title: '姓名',
+      dataIndex: 'name',
+    }, {
+      title: '日期',
+      dataIndex: 'date',
+    }, {
+      title: '地址',
+      dataIndex: 'address',
+    },{
+      title: '操作',
+      dataIndex: 'operation',
+      render: (text, record) => {
+        return (
+          this.state.data.length > 1 ?
+          (
+            <Popconfirm title="确定删除么?" onConfirm={() => this.onDelete(record.key)}>
+                <Button type="danger">删除</Button>
+            </Popconfirm>
+          ) : null
+        );
+      },
+    }];
+  }
+  state = {
+    data : userData.list
+  };
+  onDelete(key){
+    const dataSource = [...this.state.data];
+    this.setState({ data: dataSource.filter(item => item.key !== key) });
   }
   render() {
+    const rowSelection = {
+      onChange: (selectedRowKeys, selectedRows) => {
+        console.log(selectedRowKeys);
+      }
+    };
     return (
       <div>
           <Bread desArr={["ant" , "表格"]} iconType="file-text" />
+          <Table rowSelection={rowSelection} columns={this.columns} dataSource={this.state.data} />
       </div>
     )
   }
 }
 
-export default connect()(Table)
+//导入样式
+require('../../styles/ant/table.scss')
+
+export default connect()(TablePage)
