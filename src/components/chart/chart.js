64832ac0590693f7ae6d33236b6b8ac3110406e1
skycolor <873276172@qq.com>
2018-02-22 10:50:51 +0800

    引入相关组件，加入部分功能 1、图表 2、编辑器

--- src/components/chart/chart.js
+++ src/components/chart/chart.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import {connect} from 'react-redux'
+import { Row , Col } from 'antd';
+import ReactEcharts from 'echarts-for-react';
 
 import Bread from '../include/bread'
+
+//引入echart opt库
+import echartOpts from '../../lib/echartOpts';
 
 class Chart extends React.Component {
   constructor(props) {
@@ -9,11 +14,61 @@ class Chart extends React.Component {
   }
   render() {
     return (
-      <div>
+      <div className="chart-container" >
           <Bread desArr={["图表"]} iconType="star-o" />
+          <div className="tit" >
+            此处使用的一个echart的react封装库，使用方式同echart，比较简单
+            访问地址为：
+            <a href="https://github.com/hustcc/echarts-for-react" >echarts-for-react</a>
+          </div>
+          <Row>
+            <Col span={12}>
+              <ReactEcharts
+                option={echartOpts.getOpt1()}
+                style={{height: '300px'}}
+              />
+            </Col>
+            <Col span={12}>
+              <ReactEcharts
+                option={echartOpts.getOpt2()}
+                style={{height: '300px'}}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col span={12}>
+              <ReactEcharts
+                option={echartOpts.getOpt3()}
+                style={{height: '300px'}}
+              />
+            </Col>
+            <Col span={12}>
+              <ReactEcharts
+                option={echartOpts.getOpt4()}
+                style={{height: '300px'}}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col span={12}>
+              <ReactEcharts
+                option={echartOpts.getOpt5()}
+                style={{height: '300px'}}
+              />
+            </Col>
+            <Col span={12}>
+              <ReactEcharts
+                option={echartOpts.getOpt6()}
+                style={{height: '300px'}}
+              />
+            </Col>
+          </Row>
       </div>
     )
   }
 }
+
+//导入样式
+require('../../styles/chart/chart.scss')
 
 export default connect()(Chart)
--- src/components/tool/editor.js
+++ src/components/tool/editor.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import {connect} from 'react-redux'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; // ES6
 
 import Bread from '../include/bread'
 
 class Editor extends React.Component {
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this)
+  }
+  state = {
+    text: '',
+  };
+  handleChange(value) {
+    this.setState({ text: value })
   }
   render() {
     return (
-      <div>
+      <div className="editor-container" >
           <Bread desArr={["工具" , "编辑器"]} iconType="tool" />
+          <div className="tit" >
+            一款基于Quill的react富文本编辑器组件，访问地址为：
+            <a href="https://github.com/zenoamaro/react-quill" >react-quill</a>
+          </div>
+          <ReactQuill value={this.state.text}
+                  style={{height: '380px'}}
+                  onChange={this.handleChange} />
       </div>
     )
   }
 }
+
+//导入样式
+require('../../styles/tool/editor.scss')
 
 export default connect()(Editor)
