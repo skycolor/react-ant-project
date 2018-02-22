3ce9e3295ff1b3b33d94e3530db861d712fc2735
skycolor <873276172@qq.com>
2018-02-22 22:33:20 +0800

    加入注释

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import {connect} from 'react-redux'
+/*
+此处不使用BrowserRouter，是因为它依赖于服务器，如果使用npm dist打包，在本地打开dist中的
+index.html那么显示为空，主要是因为此处router不可用。所以用HashRouter的方式
+参考的博客为 https://www.cnblogs.com/iny7/p/5485357.html
+*/
 import {
-  HashRouter as Router , Route 
+  HashRouter as Router , Route
 } from 'react-router-dom'
 
 //引入router相关页面
